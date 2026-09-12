213b46c4c4eb4f8f11e7ab5a4aeb5ebce292ae77
Leonardo Ponce <xfuture.system@gmail.com>
2026-09-12 12:43:18 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev
 export default defineConfig({
-  base: '/EugeGallegosElectricidad/', // <-- Asegúrate de que este sea el nombre exacto de tu repositorio en GitHub, respetando mayúsculas y minúsculas
+  base: '/EugeGallegosElectricidad/', // <-- Asegúrate de escribirlo exactamente igual que tu repositorio
   plugins: [react()],
 })
